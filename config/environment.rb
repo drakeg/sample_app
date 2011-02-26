7557868000d06e48d7cf5ae21868cdcddabda626
Greg Drake <greg@madmallards.com>
2011-02-26 00:23:13 -0500

    Added mail configs to environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,15 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.raise_delivery_errors = true
 
 # Initialize the rails application
 SampleApp::Application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :address  => "smtp.drake.whsites.net",
+  :port  => 25,
+  :user_name  => "drakeg",
+  :password  => "6tfc5rdx",
+  :authentication  => :login
+}
